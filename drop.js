e88361e2ee4b8a13f4d54511ec03813ac079a964
hitesh78766 <michitesh6@gmail.com>
2025-03-18 16:49:54 +0530

    update the code of drag and drop function and upload image

--- drop.js
+++ drop.js
@@ -105,23 +105,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // open the prompt box to set the width of the container 
 
-    dropAreas.forEach((btn,index) =>{
-        btn.addEventListener("dblclick" , () =>{
+    dropAreas.forEach((btn, index) => {
+        btn.addEventListener("dblclick", () => {
             const userInput = prompt("Please enter the numbrer to set the width of the container")
             const columnSize = parseInt(userInput);
             // console.log("the column size is : " , columnSize)
 
-            if(!isNaN(columnSize) && columnSize >= 1 && columnSize <= 12){
+            if (!isNaN(columnSize) && columnSize >= 1 && columnSize <= 12) {
                 let newSize = `col-${columnSize}`
                 dropAreas[index].classList = `${newSize} p-0 bg-primary droppable position-relative`
-            }else{
+            } else {
                 alert("you did not enter the number to set the width of the container")
             }
 
         })
     })
-
-
 
     // const editBtns = document.querySelectorAll(".edit-btn");
 
@@ -139,17 +137,109 @@ document.addEventListener("DOMContentLoaded", () => {
     //         }
     //     })
     // })
-    
+
 });
 
 
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const editIcon = document.querySelectorAll(".edit-icon")
+    const uploadImage = document.getElementById("uploadImage");
+    console.log("the upload image is :", uploadImage);
+    const previewImage = document.getElementById("previewImage")
+    const saveImage = document.getElementById("saveImage")
+    let modal = document.getElementById("exampleModal");
+    // variable for dropzone
+    const dropZone = document.getElementById("dropZone")
+    // this variable is for set the new image in container 
+    let selectedImageContainer = null
+
+    editIcon.forEach((button) => {
+        button.addEventListener("click", () => {
+            selectedImageContainer = button.parentElement.querySelector("img");
+            console.log("the select Image container is :", selectedImageContainer)
+            previewImage.style.display = "none"; // not display preview image till then the new image is select
+            uploadImage.value = "";  // clear the previous file
+        })
+    })
+
+    // add event on upload Image 
+    uploadImage.addEventListener("change", (e) => {
+        let file = e.target.files[0]
+        console.log("the file is :", file)
+
+        if (file && file.type.startsWith("image/")) {
+            let reader = new FileReader(); // Convert the image into a displayable format
+
+            reader.onload = (e) => {
+                console.log("the event is :", e)
+                previewImage.src = e.target.result; // set the img src. in preview image
+                console.log("the preview imaeg data is :", previewImage)
+                previewImage.style.display = "block"; // show preview image 
+            };
+            reader.readAsDataURL(file); // Convert the image file into a URL
+        } else {
+            alert("Please upload a valid image file");
+        }
+
+    })
+
+    // this is the code of the drag and drop image 
+    dropZone.addEventListener("dragover" , (e) =>{
+        console.log("the dragOver is :" , e)
+        e.preventDefault()
+        dropZone.classList.add("drag-over")
+    })
+
+    dropZone.addEventListener("dragleave" , () =>{
+        dropZone.classList.remove("drag-over")
+    })
+
+    dropZone.addEventListener("drop" , (e) =>{
+        console.log("the drop image is :" , e)
+        e.preventDefault()
+        dropZone.classList.remove("drag-over")
+
+        let file = e.dataTransfer.files[0]
+        console.log("the drop file is : " , file)
+
+        if(file && file.type.startsWith("image/")){
+            let reader = new FileReader() // convert the image into a displayable format
+
+            reader.onload = (e) =>{
+                console.log("the event of drag and drop is :" . e)
+                previewImage.src = e.target.result
+                console.log("the preview image of dreg and drop is :" , previewImage)
+                previewImage.style.display = "block" // show the preview image 
+            };
+            reader.readAsDataURL(file)
+        }else{
+            alert("please upload a valid image file ")
+        }
+    })
 
 
+    // show the upload image on the click of save button
+    saveImage.addEventListener("click", () => {
+        let previewSrc = previewImage.src // get the source of preview image 
+
+        // If an image was selected and a preview exists
+        if (selectedImageContainer && previewSrc) {
+            selectedImageContainer.src = previewSrc // replace the select image 
+            closeModal(modal); // Close modal
+        }
+    })
 
 
+    function closeModal(modal) {
+        let modalInstance = bootstrap.Modal.getInstance(modal); // Get the Bootstrap modal instance
+        if (modalInstance) {
+            modalInstance.hide(); // Hide the modal
+        }
+    }
 
-
+})
 
 
 
